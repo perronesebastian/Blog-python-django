a202c2c4b34cde655d4c6bf7b7b63bf254c73bc7
“Silvana942git config --global user.email “silvanab942@gmail.com <silvanab942@gmail.com;git statusgit config --global user.name Silvana>
2021-12-21 19:01:58 -0300

    implementacion registro html salir img forms y tests

---
+++ aplicaciones/blog/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.db.models import fields
+from django.views.generic.edit import CreateView
+from .models import Categorias, Post, Usuario, Comentarios
+from ckeditor.fields import RichTextField
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['estado', 'autor']
+
+class UsuarioForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UsuarioActualizarForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ComentariosForm(forms.ModelForm):
+    class Meta:
+        model = Comentarios
+        fields = ('texto',)
+
+
+
+
