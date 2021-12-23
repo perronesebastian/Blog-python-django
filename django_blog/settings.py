83be70eef1eb0a543f9b7649030fa8b604e0b4a6
perronesebastian <perronesebastian@hotmail.com>
2021-12-22 21:22:41 -0300

    primer commit

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*qr25rvu7)uy)knisl_mi-%jy^b$ry$1wq%3z9n=u5!g166)$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('ENVIROMENT') == 'produccion':
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -126,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
@@ -140,3 +145,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog:inicio'
 
 LOGIN_URL = 'blog:acceder'
+
+if not DEBUG:
+		import django_heroku
+		django_heroku.settings(locals())
